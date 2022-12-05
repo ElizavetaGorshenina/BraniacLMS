3039ac16d380e5593fcee30b4865448933a005a7
ElizavetaGorshenina <liza_summer@mail.ru>
2022-12-05 23:49:38 +0300

    lesson_6 Users model

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from authapp import models as authapp_models
+
+
+@admin.register(authapp_models.CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "age", "email", "is_staff", "is_active"]
+    search_fields = ["username", "age", "email"]
+    ordering = ["username", "is_active"]
+    list_per_page = 20
+    list_filter = ["username", "age", "is_staff", "is_active"]
